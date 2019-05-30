709d50380a33a3358015847b408848c9a6fd2137
Jean Ribeiro Pinto <jeanribeirop@gmail.com>
2019-05-30 15:28:48 -0500

    Submitting homework04

---
+++ homework_04/process-chunk.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+ 
+process.on('message', (fileObject) => 
+{  
+    let dir = path.join(__dirname, fileObject.filename); 
+    var readable = fs.createReadStream(dir.toString(), 'utf8');
+    
+    readable.on('data', chunk =>
+    {         
+        process.send(chunk);
+    });
+
+    readable.on('end', (data) => { 
+        process.exit(0);
+    });
+});
---
+++ homework_04/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const { fork } = require('child_process');
+const { Subject } = require('rxjs');
+const qs = require('querystring');
+const url = require('url');
+
+const subject = new Subject(); 
+
+subject.subscribe((serverObj) =>
+ {
+    var str = serverObj.req.url.split('?')[1];
+    const childProcess = fork('process-chunk.js');
+    childProcess.send(qs.parse(str));  
+  
+    childProcess.on('message', chunk => 
+    {
+        serverObj.res.write(chunk.toString()); 
+    });
+
+    childProcess.on('exit', () => 
+    { 
+        serverObj.res.end();
+    });
+})
+
+http.createServer((req, res) => {
+    subject.next({ req: req, res: res })
+}).listen(4000);
